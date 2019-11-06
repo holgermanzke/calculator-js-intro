0467e714f0c9f783cb5afcd9a61b68453979939a
holgermanzke <manzkeholger@gmail.com>
2019-11-06 15:17:57 +0100

    add function for digit counter

--- src/lib/operators.js
+++ src/lib/operators.js
@@ -13,3 +13,21 @@ export function multi(a, b) {
 export function div(a, b) {
   return a / b;
 }
+
+export function countDigits(n) {
+  let count = 0;
+  if (n < 0) {
+    ++count;
+    n = n * -1;
+  }
+  if (n >= 0) ++count;
+  while (n / 10 >= 1) {
+    n /= 10;
+    ++count;
+  }
+  return count;
+}
+console.log(countDigits(12112));
+
+console.log(countDigits(457));
+console.log(countDigits(-42));
