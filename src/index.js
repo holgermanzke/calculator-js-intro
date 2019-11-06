215cc21f921411fc86d4139f1f384853699f9995
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-11-06 15:55:58 +0100

    add a button fpr digit counter (#6)

--- src/index.js
+++ src/index.js
@@ -6,22 +6,26 @@ const firstInput = document.querySelector("[name=first]");
 const secondInput = document.querySelector("[name=second]");
 const sumButton = document.querySelector(".sum");
 const result = document.querySelector("#result");
-const operator = document.querySelector(".operator");
+const operator = document.querySelector("#operator");
 const substractionButton = document.querySelector(".substraction");
 const multiplyButton = document.querySelector(".multiply");
 const divideButton = document.querySelector(".divide");
+const digits = document.querySelector("#digits");
+// const counterButton = document.querySelector(".counter");
 
 sumButton.addEventListener("click", function() {
   // const sum = parseInt(firstInput.value) + parseInt(secondInput.value);
   const firstNumber = parseInt(firstInput.value);
   const secondNumber = parseInt(secondInput.value);
   result.innerHTML = sum(firstNumber, secondNumber);
+  digits.innerHTML = countDigits(sum(firstNumber, secondNumber));
 });
 
 substractionButton.addEventListener("click", function() {
   const firstNumber = parseInt(firstInput.value);
   const secondNumber = parseInt(secondInput.value);
   result.innerHTML = sub(firstNumber, secondNumber);
+  digits.innerHTML = countDigits(sub(firstNumber, secondNumber));
   operator.innerHTML = "-";
 });
 
@@ -29,6 +33,7 @@ multiplyButton.addEventListener("click", function() {
   const firstNumber = parseInt(firstInput.value);
   const secondNumber = parseInt(secondInput.value);
   result.innerHTML = multi(firstNumber, secondNumber);
+  digits.innerHTML = countDigits(multi(firstNumber, secondNumber));
   operator.innerHTML = "x";
 });
 
@@ -36,5 +41,6 @@ divideButton.addEventListener("click", function() {
   const firstNumber = parseInt(firstInput.value);
   const secondNumber = parseInt(secondInput.value);
   result.innerHTML = div(firstNumber, secondNumber);
+  digits.innerHTML = countDigits(div(firstNumber, secondNumber));
   operator.innerHTML = ":";
 });
