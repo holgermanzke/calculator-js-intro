8bba9fb54c21097b9d1f29a50e8f9d5fc2d17e07
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-11-06 13:51:58 +0100

    add operators js (#3)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
+"use strict";
+
+import { sum, sub, multi, div } from "./lib/operators";
+
 const firstInput = document.querySelector("[name=first]");
 const secondInput = document.querySelector("[name=second]");
-const submitButton = document.querySelector(".submit");
+const sumButton = document.querySelector(".sum");
+const result = document.querySelector("#result");
+const operator = document.querySelector(".operator");
+const substractionButton = document.querySelector(".substraction");
+const multiplyButton = document.querySelector(".multiply");
+const divideButton = document.querySelector(".divide");
 
-submitButton.addEventListener("click", function() {
-  const sum = parseInt(firstInput.value) + parseInt(secondInput.value);
-  alert(sum);
+sumButton.addEventListener("click", function() {
+  // const sum = parseInt(firstInput.value) + parseInt(secondInput.value);
+  const firstNumber = parseInt(firstInput.value);
+  const secondNumber = parseInt(secondInput.value);
+  result.innerHTML = sum(firstNumber, secondNumber);
+});
+
+substractionButton.addEventListener("click", function() {
+  const firstNumber = parseInt(firstInput.value);
+  const secondNumber = parseInt(secondInput.value);
+  result.innerHTML = sub(firstNumber, secondNumber);
+  operator.innerHTML = "-";
+});
+
+multiplyButton.addEventListener("click", function() {
+  const firstNumber = parseInt(firstInput.value);
+  const secondNumber = parseInt(secondInput.value);
+  result.innerHTML = multi(firstNumber, secondNumber);
+  operator.innerHTML = "x";
+});
+
+divideButton.addEventListener("click", function() {
+  const firstNumber = parseInt(firstInput.value);
+  const secondNumber = parseInt(secondInput.value);
+  result.innerHTML = div(firstNumber, secondNumber);
+  operator.innerHTML = ":";
 });
